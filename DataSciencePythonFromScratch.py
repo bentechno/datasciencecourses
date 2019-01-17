79c0cb64d9cbac1b5644d7c77d04ef95e15e82fc
bentechno <mbayebabacar@gmail.com>
2019-01-17 00:39:34 -0500

    addDataDescription

--- DataSciencePythonFromScratch.py
+++ DataSciencePythonFromScratch.py
@@ -5,4 +5,10 @@ Created on Wed Jan 16 23:26:38 2019
 
 @author: mbaye
 """
+import pandas as pd
+import numpy as np
+import matplotlib as plt
 
+df = pd.read_csv("/media/mbaye/DATA/mescours/datascience/dev/datasciencecourses/data/train.csv")
+df.head(10)
+df.describe()
